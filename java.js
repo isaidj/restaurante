93dd99eab4e575dfd6c3b9662ce73ffba38aa60e
isaidj <44456363+isaidj@users.noreply.github.com>
2020-11-14 19:39:23 -0500

    7:39

--- java.js
+++ java.js
@@ -7,3 +7,12 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+  /* Set the width of the side navigation to 250px */
+function openNavRight() {
+  document.getElementById("mySidenavRight").style.height = "500px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNavRight() {
+  document.getElementById("mySidenavRight").style.height = "0";
+}
