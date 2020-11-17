a0aa0ff2229c43066e4352cf48d0103dde9016d1
isaidj <44456363+isaidj@users.noreply.github.com>
2020-11-16 20:11:24 -0500

    lunes

--- java.js
+++ java.js
@@ -1,18 +1,57 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "280px";
+/* Variable Publicas*/
+let estado=false;
+function openNavleft() {
+  /* Set the width of the side navigation to 250px */
+    document.getElementById("mySidenav").style.width = "35vh";
   }
   
   /* Set the width of the side navigation to 0 */
-  function closeNav() {
+  function closeNavleft() {
     document.getElementById("mySidenav").style.width = "0";
   }
   /* Set the width of the side navigation to 250px */
 function openNavRight() {
-  document.getElementById("mySidenavRight").style.height = "500px";
+  document.getElementById("mySidenavRight").style.height = "20vh";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNavRight() {
   document.getElementById("mySidenavRight").style.height = "0";
 }
+
+function validarVentana(){
+  if(estado){
+    let navderecha =  document.getElementById("mySidenavRight").style.height= "0vh";
+    estado=false;
+  }else{
+    let navderecha =  document.getElementById("mySidenavRight").style.height= "20vh";
+    estado=true;
+  }
+  
+  
+
+  
+ 
+  
+}
+
+
+
+
+
+/*
+let navderecha =  document.getElementById("mySidenavRight");
+navderecha.addEventListener ('click' ,()=>{
+
+navderecha.style = 'height :20vh';
+
+
+
+  
+})*/
+
+
+
+
+
+
